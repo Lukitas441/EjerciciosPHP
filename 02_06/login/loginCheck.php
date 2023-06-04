db62cd6d46b1741128bf889f0147eaaea3a6df4c
Lukitas <lucas.r.abimorad@gmail.com>
2023-06-03 22:14:39 -0300

    Se agrego codigo html chequeo del login para hacerlo mas estetico

--- 02_06/login/loginCheck.php
+++ 02_06/login/loginCheck.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     include 'User.php';
     $usuarios = [
@@ -10,18 +20,25 @@
     if($_GET !== 'user' && $_GET['password'] && isset($_GET['user']) && isset($_GET['password'])){
         $name = $_GET['user'];
         $password = $_GET['password'];
-     
+        
+        $userCorrecto = false;
         foreach($usuarios as $usuario){
             if($usuario->getName() == $name){
+                $userCorrecto = true;
                 if($password == $usuario->getPassword()) {
                     echo '<h1>Bienvenido '.$usuario->getName().'!</h1>';
                     break;
                 } else {
-                    echo 'Contraseña no valida';
+                    echo '<h3>Contraseña no valida</h3>';
                 }
             }
+        }
+        if($userCorrecto != true) {
+            echo '<h3>Usuario no encontrado</h3>';
         }
     } else {
         echo '<h1>Inicie sesion!</h1>';
     }
 ?>
+</body>
+</html>
